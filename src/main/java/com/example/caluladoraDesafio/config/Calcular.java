1753a2fd4063056ec310f5340d32ab1423d42801
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-31 09:33:00 -0400

    feat(Calcular): calculo de media

--- src/main/java/com/example/caluladoraDesafio/config/Calcular.java
+++ src/main/java/com/example/caluladoraDesafio/config/Calcular.java
@@ -1,4 +1,19 @@
 package com.example.caluladoraDesafio.config;
 
+import java.util.List;
+
 public class Calcular {
+    public static double media(List<Double> valores){
+        if (valores.isEmpty()) {
+            throw new IllegalArgumentException("A lista de valores não pode estar vazia.");
+        }
+        double total = 0;
+
+        //Para cada valor dentro de valores, soma-o ao total
+        for (Double valor: valores) {
+            total += valor;
+        }
+
+        return total / valores.size();
+    }
 }
