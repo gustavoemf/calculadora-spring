6569297e54c0c9f5d647d3c152db7ae9e5d191ba
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-31 11:26:51 -0400

    feat(test): teste desvio padrao

--- src/main/java/com/example/caluladoraDesafio/service/ValorService.java
+++ src/main/java/com/example/caluladoraDesafio/service/ValorService.java
@@ -1,18 +1,69 @@
 package com.example.caluladoraDesafio.service;
 
-import com.example.caluladoraDesafio.config.Calcular;
-import com.example.caluladoraDesafio.dto.Valor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
 public class ValorService {
-    public String calcular(List<Double> valores){
-        return "Quantidade de dados recebidos: " + valores.size() + "\nMédia: " + Calcular.media(valores) + "\nDesvio padrão: " + Calcular.desvioPadrao(valores) + "\nMediana: " + Calcular.mediana(valores);
+    public static double somar(List<Double> valores){
+        double total = 0;
+        for (Double valor: valores) {
+            total += valor;
+        }
+        return total;
     }
+    public static double media(List<Double> valores){
+        if (valores.isEmpty()) {
+            throw new IllegalArgumentException("A lista de valores não pode estar vazia.");
+        }
+        double total = 0;
 
-    public Valor coletarDados(List<Double> valores) {
-        return new Valor(valores);
+        //Para cada valor dentro de valores, soma-o ao total
+        for (Double valor: valores) {
+            total += valor;
+        }
+
+        return total / valores.size();
+    }
+    public static double desvioPadrao(List<Double> valores){
+        if (valores.isEmpty()) {
+            throw new IllegalArgumentException("A lista de valores não pode estar vazia.");
+        }
+        //Faz o cálculo da média
+        double media = media(valores);
+
+        double somaDosQuadradosDasDiferencas = 0.0;
+        for (Double valor : valores) {
+            somaDosQuadradosDasDiferencas += Math.pow(valor - media, 2);
+        }
+
+        double variancia = somaDosQuadradosDasDiferencas / (valores.size());
+        return Math.sqrt(variancia);
+    }
+    public static double mediana(List<Double> valores){
+        if (valores.isEmpty()) {
+            throw new IllegalArgumentException("A lista de valores não pode estar vazia.");
+        }
+        //Ordenar valores em ordem
+        Collections.sort(valores);
+
+        int tamanho = valores.size();
+
+        //Verifica se o valor da mediana é ímpar/único, se não faz o cálculo de divisão
+        if (tamanho % 2 != 0) {
+            int indiceMediana = tamanho / 2;
+            return valores.get(indiceMediana);
+        } else {
+            int indiceMediana1 = (tamanho - 1) / 2;
+            int indiceMediana2 = tamanho / 2;
+            double mediana1 = valores.get(indiceMediana1);
+            double mediana2 = valores.get(indiceMediana2);
+            return (mediana1 + mediana2) / 2;
+        }
+    }
+    public String calcular(List<Double> valores){
+        return "Quantidade de dados recebidos: " + valores.size() + "\nMédia: " + media(valores) + "\nDesvio padrão: " + desvioPadrao(valores) + "\nMediana: " + mediana(valores);
     }
 }
--- src/test/java/com/example/caluladoraDesafio/CaluladoraDesafioApplicationTests.java
+++ src/test/java/com/example/caluladoraDesafio/CaluladoraDesafioApplicationTests.java
@@ -1,13 +1,12 @@
 package com.example.caluladoraDesafio;
 
-import com.example.caluladoraDesafio.config.Calcular;
-import org.junit.Assert;
+import com.example.caluladoraDesafio.service.ValorService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -18,35 +17,40 @@ class CaluladoraDesafioApplicationTests {
 
 	@BeforeEach
 	void gerarLista(){
-		valores = Arrays.asList(
-				20.0, 21.0, 22.0, 23.0, 24.0, 25.0, 26.0, 27.0, 28.0, 29.0,
-				30.0, 31.0, 32.0, 33.0, 34.0, 35.0, 36.0, 37.0, 38.0, 39.0, 40.0
-		);
+		ValorService service = new ValorService();
+		for (int i = 1; i <= 20; i++) {
+			valores.add((double) i);
+		}
 		desvio = 0.001;
 	}
 	@Test
 	public void testSoma(){
-		double esperado = 630.0; // Soma dos valores
-		double resultado;
-		resultado = Calcular.somar(valores);
-		Assert.assertEquals(esperado, resultado, desvio);
+		double esperado = 210; // Soma dos valores
+		double resultado = ValorService.somar(valores);
+		Assertions.assertEquals(esperado, resultado, desvio);
 	}
 	@Test
 	public void testValorMax(){
-		double esperado = 40.0; // Maior valor na lista
+		double esperado = 20; // Maior valor na lista
 		double resultado = Collections.max(valores);
-		Assert.assertEquals(esperado, resultado, desvio);
+		Assertions.assertEquals(esperado, resultado, desvio);
 	}
 	@Test
 	public void testValorMin(){
-		double esperado = 20.0; // Menor valor na lista
+		double esperado = 1; // Menor valor na lista
 		double resultado = Collections.min(valores);
-		Assert.assertEquals(esperado, resultado, desvio);
+		Assertions.assertEquals(esperado, resultado, desvio);
 	}
 	@Test
 	public void testMetodoMedia(){
-		double esperado = 30; // Média dos valores
-		double resultado = Calcular.media(valores);
-		Assert.assertEquals(esperado, resultado, desvio);
+		double esperado = 10.5; // Média dos valores
+		double resultado = ValorService.media(valores);
+		Assertions.assertEquals(esperado, resultado, desvio);
+	}
+	@Test
+	public void testMetodoDesvioPadrao(){
+		double esperado = 5.766281297335398; // Desvio padrão dos valores
+		double resultado = ValorService.desvioPadrao(valores);
+		Assertions.assertEquals(esperado, resultado, desvio);
 	}
 }
