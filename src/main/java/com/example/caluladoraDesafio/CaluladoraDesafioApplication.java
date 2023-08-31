44457c72278f7e600ba2d2332d0ef575fdab1d65
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-31 09:25:52 -0400

    first commit

---
+++ src/main/java/com/example/caluladoraDesafio/CaluladoraDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.example.caluladoraDesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaluladoraDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaluladoraDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/caluladoraDesafio/CaluladoraDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caluladoraDesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaluladoraDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
