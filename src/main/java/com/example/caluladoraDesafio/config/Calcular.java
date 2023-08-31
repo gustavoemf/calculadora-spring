e9b1e29f32d7730e8a99c997cab7dbe0a3594238
gustavoemf <gustavoeduardomf@gmail.com>
2023-08-31 09:33:22 -0400

    feat(Calcular): calculo de desvio padrao

--- src/main/java/com/example/caluladoraDesafio/config/Calcular.java
+++ src/main/java/com/example/caluladoraDesafio/config/Calcular.java
@@ -16,4 +16,20 @@ public class Calcular {
 
         return total / valores.size();
     }
+    public static double desvioPadrao(List<Double> valores){
+        if (valores.isEmpty()) {
+            throw new IllegalArgumentException("A lista de valores não pode estar vazia.");
+        }
+        //Faz o cálculo da média
+        double media = media(valores);
+
+        double somaDosQuadradosDasDiferencas = 0.0;
+        for (double valor : valores) {
+            double diferenca = valor - media;
+            somaDosQuadradosDasDiferencas += diferenca * diferenca;
+        }
+
+        double variancia = somaDosQuadradosDasDiferencas / (valores.size() - 1);
+        return Math.sqrt(variancia);
+    }
 }
